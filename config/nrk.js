c6308b28a9cda6a9fbeeff17b2fcb6e8febb93fc
Dominik Klauser <klauser.dominik@aon.at>
2024-06-13 15:20:11 +0200

    update

--- config/nrk.js
+++ config/nrk.js
@@ -69,8 +69,8 @@ module.exports = {
 
         if(rktQualifications.length === 1) {
             switch (rktQualifications[0]['qualifikation_id']) {
-                case 1: return 'RS';
-                case 2: return 'NFS';
+                case 1: return 'Rettungs&shy;sanitäter:in';
+                case 2: return 'Notfall&shy;sanitäter:in';
                 case 3: return 'NFS NKA';
                 case 4: return 'NFS NKV';
                 case 5: 
